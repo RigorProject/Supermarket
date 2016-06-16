c6723693e73dc0e7562762b74b9e3ec38fb82b66
Madhukara Kekulandara <mkekulandara@gmail.com>
2016-06-16 10:21:28 +0530

    Single page interface
    iframe based interface

---
+++ Implementation - MK - 01/SuperMarket/src/com/rigor/configuration/AppConfig.java
@@ -0,0 +1,28 @@
+package com.rigor.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.rigor")
+public class AppConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+	
+
+}
+
